a1df6e243423a6e7fc14c61d5ee16eabc4690db2
Yash verma <yashverma9084+github@gmail.com>
2026-05-26 11:17:32 +0530

    Protrct url

---
+++ Task_Manager_API/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const protect = async (req, res, next) => {
+    try {
+        let token;
+        if (req.header.authorization && req.header.authorization.startWith("Bearer")) {
+            token = req.header.authorization.split(" ")[1]
+        }
+        if (!token) {
+            return res.status(401).json({ message: "No Token found" })
+        }
+        const decode = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        )
+        req.user = decode
+        next()
+    } catch (err) {
+        res.status(401).json({
+            message: "Invalid token"
+        });
+    }
+}
+module.exports = protect
